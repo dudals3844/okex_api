ea9957a2368145c478f44af8002f9d7f3ae7baa1
YoungMin <dudals3844@gmail.com>
2020-09-20 22:57:34 +0900

    add Ticker.kt

---
+++ src/main/kotlin/com/okex/api/data/futures/Ticker.kt
@@ -0,0 +1,18 @@
+package com.okex.api.data.futures
+
+import lombok.Data
+
+@Data
+data class Ticker(
+        val instrument_id: String,
+        val best_ask: String,
+        val best_bid: String,
+        val last: String,
+        val high_24h: String,
+        val low_24h: String,
+        val volume_24h: String,
+        val timestamp: String,
+        val last_qty: String,
+        val best_bid_size: String,
+        val best_ask_size: String
+)
