4d58aa8d4113b24cdc0596857fb9dba8feff4b67
YoungMin <dudals3844@gmail.com>
2020-09-21 00:49:56 +0900

    make Const.kt

--- src/main/kotlin/com/okex/api/ApiApplication.kt
+++ src/main/kotlin/com/okex/api/ApiApplication.kt
@@ -2,10 +2,14 @@ package com.okex.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import com.okex.api.const.*
 
 @SpringBootApplication
 class ApiApplication
 
 fun main(args: Array<String>) {
     runApplication<ApiApplication>(*args)
+    underlying = "btc-usdt"
+    println(underlying)
+    println(GET_FUTURES_LEVERAGE)
 }
---
+++ src/main/kotlin/com/okex/api/const/Const.kt
@@ -0,0 +1,58 @@
+package com.okex.api.const
+
+const val BASE_URL = "https://www.okex.com/"
+
+const val FUTURE_POSITIONS = "api/futures/v3/position"
+
+const val FUTURES_ACCOUNT_BASE_URL= "api/futures/v3/accounts"
+
+const val FUTURES_ORDER_BASE_URL = "api/futures/v3/order"
+
+const val FUTURES_MODIFY_ORDER = "api/futures/v3/amend_order"
+
+const val FUTURES_BATCH_MODIFY_ORDER = "api/futures/v3/amend_batch_orders"
+
+const val FUTURES_CANCLE_ORDER = "api/futures/v3/cancel_order"
+
+const val FUTURES_BATCH_CANCLE_ORDER = "api/futures/v3/cancel_batch_orders"
+
+const val FUTURES_ORDER_LIST_BASE_URL = "api/futures/v3/orders"
+
+const val FUTURES_TRANSACTION_DETAILS = "api/futures/v3/fills"
+
+const val FUTURES_SETTING_ACCOUNT_MODE = "api/futures/v3/accounts/margin_mode"
+
+const val FUTURES_CURRENT_TRANSACTION_FEE_RATE = "api/futures/v3/trade_fee"
+
+const val FUTURES_MARKET_CLOSE_ALL = "api/futures/v3/close_position"
+
+const val FUTURES_CANCLE_ALL_POSITION = "api/futures/v3/cancel_all "
+
+const val FUTURES_INCREASE_DECREASE_MARGIN = "api/futures/v3/position/margin"
+
+const val FUTURES_PUBLIC_DATA_BASE_URL = "api/futures/v3/instruments"
+
+const val FUTURES_EXCHANGE_RATES = "api/futures/v3/rate"
+
+const val FUNDING_ACCOUNT_BASE_URL = "api/account/v3/wallet"
+
+const val SUB_ACCOUNT = "api/account/v3/sub-account"
+
+const val ASSET_VALUATION = "api/account/v3/asset-valuation"
+
+const val FUND_TRANSFER = "api/account/v3/transfer"
+
+const val WITHDRAWAL = "api/account/v3/withdrawal"
+
+const val WITHDRAWAL_HISTORY = "api/account/v3/withdrawal/history"
+
+const val BILL_DETAILS = "api/account/v3/ledger"
+
+const val DEPOSIT_ADDRESS = "api/account/v3/deposit/address"
+
+const val DEPOSIT_HISTORY = "api/account/v3/deposit/history"
+
+const val GET_CURRENCIES = "api/account/v3/currencies"
+
+const val WITHDRAWAL_FEE = "api/account/v3/withdrawal/fee"
+
